2a50e4c58d92117fda4b3cb905d5b3bdb4211800
hamuhamu <hrdng4922000@yahoo.co.jp>
2018-02-28 18:40:07 +0900

    IteratorExampleにPHPDocを追加した

--- src/IteratorExample.php
+++ src/IteratorExample.php
@@ -11,37 +11,63 @@ namespace App;
 class IteratorExample implements \Iterator
 {
     private $position = 0;
-    private $array = array(
+    private $array = [
         "firstelement",
         "secondelement",
         "lastelement",
-    );
+    ];
 
     public function __construct() {
         $this->position = 0;
     }
 
-    public function rewind() {
+    /**
+     * Iterator::rewind — イテレータの最初の要素に巻き戻す
+     */
+    public function rewind(): void
+    {
         var_dump(__METHOD__);
         $this->position = 0;
     }
 
-    public function current() {
+    /**
+     * Iterator::current — 現在の要素を返す
+     *
+     * @return mixed
+     */
+    public function current()
+    {
         var_dump(__METHOD__);
         return $this->array[$this->position];
     }
 
-    public function key() {
+    /**
+     * Iterator::key — 現在の要素のキーを返す
+     *
+     * @return int
+     */
+    public function key(): int
+    {
         var_dump(__METHOD__);
         return $this->position;
     }
 
-    public function next() {
+    /**
+     * Iterator::next — 次の要素に進む
+     */
+    public function next(): void
+    {
         var_dump(__METHOD__);
         ++$this->position;
     }
 
-    public function valid() {
+    /**
+     * Iterator::valid — 現在位置が有効かどうかを調べる
+     *
+     * @return bool
+     */
+    public function valid(): bool
+    {
         var_dump(__METHOD__);
         return isset($this->array[$this->position]);
     }
