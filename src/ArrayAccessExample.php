92335ee550d2a9580c062e398451c3b8b3c59b89
hamuhamu <hrdng4922000@yahoo.co.jp>
2018-02-28 16:36:40 +0900

    ArrayAccessExampleにPHPDocを追加した

--- src/ArrayAccessExample.php
+++ src/ArrayAccessExample.php
@@ -4,26 +4,45 @@ namespace App;
 
 class ArrayAccessExample implements \ArrayAccess
 {
-//* ArrayAccess::offsetExists — オフセットが存在するかどうか
-//* ArrayAccess::offsetGet — オフセットを取得する
-//* ArrayAccess::offsetSet — 指定したオフセットに値を設定する
-//* ArrayAccess::offsetUnset — オフセットの設定を解除する
-
+    /**
+     * ArrayAccess::offsetExists — オフセットが存在するかどうか
+     *
+     * @param mixed $offset
+     * @return bool
+     */
     public function offsetExists($offset): bool
     {
         return true;
     }
 
+    /**
+     * ArrayAccess::offsetGet — オフセットを取得する
+     *
+     * @param mixed $offset
+     * @return bool
+     */
     public function offsetGet($offset)
     {
         return true;
     }
 
+    /**
+     * ArrayAccess::offsetSet — 指定したオフセットに値を設定する
+     *
+     * @param mixed $offset
+     * @param mixed $value
+     */
     public function offsetSet($offset, $value): void
     {
         return;
     }
 
+
+    /**
+     * ArrayAccess::offsetUnset — オフセットの設定を解除する
+     *
+     * @param mixed $offset
+     */
     public function offsetUnset($offset ): void
     {
         return;
